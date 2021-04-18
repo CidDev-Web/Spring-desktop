dc306dfd66bac78ccbbfa2ae3d80ebd9475a2a20
ciddev <ciddev.web@gmail.com>
2021-04-18 20:18:34 +0200

    spring-desktop quickstart

---
+++ src/main/java/com/ciddev/project_spring_desktop/App.java
@@ -0,0 +1,13 @@
+package com.ciddev.project_spring_desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
